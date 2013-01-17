d14bf27e8d6f780f25aef2ff5fbaf7d1d91e489c
Christian Neukirchinger <christian.neukirchinger@gmail.com>
2013-01-17 14:56:49 +0100

    Repeat now working

--- js/repeater.js
+++ js/repeater.js
@@ -1,22 +1,22 @@
-// console.log("Hello World");
-
-// function doRepeat() {
-//   console.log("Repeater Job started");
-//   var repeatInput;
-//   var repeatResult = '';
-  
-//   repeatInput = document.getElementById('txaInput').value;
-//   repeatTimes = document.getElementById('inpTimes').value;
-  
-//   for(var i = 0; i < repeatTimes; i++) {
-//     repeatResult = repeatResult.concat(i + ': ' + repeatInput.replace("@@i@@", i) + '\n');
-//   }
-//   document.getElementById('txaOutput').value = repeatResult;
-// }
 $(document).ready(function() {
-	console.log("Hello");
-	$("#btnExample").click(function() {
-		$("textarea#txaOutput").html = "Test";
-		console.log("Click");
+	console.log('Document ready');
+  
+	$('#btnExample').click(function() {
+    console.log('Click: Example');
+		$('#txaOutput').val('Test');
 	})
+  
+  $('#btnRepeat').click(function() {
+    console.log('Click: Repeat');
+    
+    var inputText = $('#txaInput').val();
+    var outputText = '';
+    var repeatTimes = $('#inpTimes').val();
+    
+    for(var i = 0; i < repeatTimes; i++) {
+      outputText = outputText.concat(inputText.replace(/@@i@@/g, i) + '\n');
+    }
+    
+    $('#txaOutput').val(outputText);
+  })
 })
